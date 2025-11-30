996e9acf3bdd20f52c631013d2c60be7bbbcdc94
Domman7 <m.egor2002@gmail.com>
2025-11-30 20:01:02 +0400

    Добавьте файлы проекта.

---
+++ WebTetris/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebTetris.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
