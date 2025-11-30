42b41367915d84eb061af7712dd0716c08423a8f
Domman7 <m.egor2002@gmail.com>
2025-12-01 00:16:13 +0400

    fix correct auth + reg

--- WebTetris/Controllers/UserController.cs
+++ WebTetris/Controllers/UserController.cs
@@ -30,14 +30,22 @@ namespace WebTetris.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginModel loginModel)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(loginModel);
+            }
+
             var user = _bll.GetByLogin(loginModel.Login);
 
             if (user != null && user.Password == loginModel.Password)
             {
                 var identity = new CustomUserIdentity(user);
                 await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+                return RedirectToAction("Index", "Home");
             }
-            return RedirectToAction("Index", "Home");
+
+            ModelState.AddModelError("", "Неверный логин или пароль");
+            return View(loginModel);
         }
 
         [HttpGet]
@@ -49,17 +57,46 @@ namespace WebTetris.Controllers
         [HttpPost]
         public async Task<IActionResult> Registration(RegistrationModel registrationModel)
         {
-            User newUser = new User()
+            if (!ModelState.IsValid)
             {
-                Name = registrationModel.Name,
-                Login = registrationModel.Login,
-                Password = registrationModel.Password,
-                RegistrationDate = DateTime.Now
-            };
-            _bll.AddUser(newUser);
-            var identity = new CustomUserIdentity(newUser);
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
-            return RedirectToAction("Index", "Home");
+                return View(registrationModel);
+            }
+
+            if (string.IsNullOrWhiteSpace(registrationModel.Name) ||
+                string.IsNullOrWhiteSpace(registrationModel.Login) ||
+                string.IsNullOrWhiteSpace(registrationModel.Password))
+            {
+                ModelState.AddModelError("", "Все поля должны быть заполнены");
+                return View(registrationModel);
+            }
+
+            var existingUser = _bll.GetByLogin(registrationModel.Login);
+            if (existingUser != null)
+            {
+                ModelState.AddModelError("Login", "Пользователь с таким логином уже существует");
+                return View(registrationModel);
+            }
+
+            try
+            {
+                User newUser = new User()
+                {
+                    Name = registrationModel.Name,
+                    Login = registrationModel.Login,
+                    Password = registrationModel.Password,
+                    RegistrationDate = DateTime.Now
+                };
+
+                _bll.AddUser(newUser);
+                var identity = new CustomUserIdentity(newUser);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", $"Произошла ошибка при регистрации: {ex.Message}");
+                return View(registrationModel);
+            }
         }
 
         public IActionResult Get(int id)
